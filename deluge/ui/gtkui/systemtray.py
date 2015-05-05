93762c7711ee79a07dbbdb8f0010acf4acd35eaf
doadin <tbkizle@gmail.com>
2015-05-05 08:47:26 -0400

    Fix Bunch of Flake8 and isort errors fourth pass

--- deluge/ui/gtkui/systemtray.py
+++ deluge/ui/gtkui/systemtray.py
@@ -15,7 +15,8 @@ import deluge.component as component
 from deluge.configmanager import ConfigManager
 from deluge.ui.client import client
 from deluge.ui.gtkui import dialogs
-from deluge.ui.gtkui.common import build_menu_radio_list, get_logo
+from deluge.ui.gtkui.common import build_menu_radio_list
+# get_logo
 from gi.repository import Gtk
 
 try:
