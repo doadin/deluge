8848fc276a4b8be0f805dc77660dc5b688f1e4b3
doadin <tbkizle@gmail.com>
2016-04-25 12:52:52 -0400

    [lang.py] Remove Gtk.glade which is no longer available

--- deluge/ui/util/lang.py
+++ deluge/ui/util/lang.py
@@ -99,10 +99,6 @@ def setup_translations(setup_gettext=True, setup_pygtk=False):
                 libintl.bind_textdomain_codeset(domain, 'UTF-8')
                 libintl.gettext.restype = ctypes.c_char_p
 
-            # Gtk.glade no longer available change plugins
-            # import Gtk.glade
-            # Gtk.glade.bindtextdomain(domain, translations_path)  # TOFIX
-            # Gtk.glade.textdomain(domain)
         except Exception as ex:
             log.error('Unable to initialize glade translation!')
             log.exception(ex)
